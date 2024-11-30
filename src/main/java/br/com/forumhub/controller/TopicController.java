66b4de119795df34b751bc6deb89c97b7ae6c058
alissonfernandes <alissonfernandes200@gmail.com>
2024-11-30 12:57:19 -0300

    Create new endpoint 'list all topics'

--- src/main/java/br/com/forumhub/controller/TopicController.java
+++ src/main/java/br/com/forumhub/controller/TopicController.java
@@ -1,26 +1,37 @@
 package br.com.forumhub.controller;
 
 import br.com.forumhub.domain.topic.dto.NewTopic;
+import br.com.forumhub.domain.topic.dto.TopicData;
+import br.com.forumhub.domain.topic.model.Topic;
+import br.com.forumhub.domain.topic.repository.TopicRepository;
 import br.com.forumhub.domain.topic.service.TopicService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/topicos")
 public class TopicController {
 
     @Autowired
-    TopicService topicService;
+    private TopicService topicService;
+
+    @Autowired
+    private TopicRepository topicRepository;
 
     @PostMapping
     @Transactional
     public ResponseEntity create(@RequestBody @Valid NewTopic newTopic) {
         return ResponseEntity.ok(topicService.createNew(newTopic));
+    }
+
+    @GetMapping
+    public ResponseEntity listAll(@PageableDefault(size = 10, sort = {"createDate"}) Pageable pageable) {
+        var page = topicRepository.findAll(pageable).map(TopicData::new);
+        return ResponseEntity.ok(page);
     }
 }
--- src/main/java/br/com/forumhub/domain/topic/dto/TopicData.java
+++ src/main/java/br/com/forumhub/domain/topic/dto/TopicData.java
@@ -1,6 +1,20 @@
 package br.com.forumhub.domain.topic.dto;
 
+import br.com.forumhub.domain.topic.model.Topic;
+
 import java.time.LocalDateTime;
 
 public record TopicData(Long id, String title, String message, LocalDateTime date, String status, Long author, Long course) {
+
+    public TopicData(Topic t) {
+        this(
+            t.getId(),
+            t.getTitle(),
+            t.getMessage(),
+            t.getCreateDate(),
+            t.getStatus(),
+            t.getAuthor().getId(),
+            t.getCourse().getId()
+        );
+    }
 }
